f8bea04687c55e222e3b4ddb30f4dce6aa02d1d5
Chris Johnson <thechrisjohnson@protonmail.com>
2025-02-05 14:50:50 -0500

    Trying out generators with fib

--- src/fibo.py
+++ src/fibo.py
@@ -15,6 +15,14 @@ def fib2(n):
         a, b = b, a+b
     return result
 
+def fib3(n):
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a+b
+
 if __name__ == "__main__":
     import sys
-    fib(int(sys.argv[1]))
+    for number in fib3(int(sys.argv[1])):
+        print(number)
+
